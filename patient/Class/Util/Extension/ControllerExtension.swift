92e3b6c96e7c85bd60b3636bb6570ec7b006837c
lixz <lixiangzhouno1@163.com>
2019-05-29 15:22:09 +0800

    删除无用代码

--- patient/Class/Util/Extension/ControllerExtension.swift
+++ patient/Class/Util/Extension/ControllerExtension.swift
@@ -18,32 +18,4 @@ extension UIViewController {
     func pop(animated: Bool = true) {
         navigationController?.popViewController(animated: animated)
     }
-    
-    func addNavigationBarEffectView() {
-        if let navBgView = navigationController?.navigationBar.subviews.first, let effectView = navigationBarEffectView {
-            navBgView.insertSubview(effectView, at: 0)
-        }
-    }
-    
-    var navigationBarEffectView: UIVisualEffectView? {
-        get {
-            var effectView = objc_getAssociatedObject(self, &navigationBarEffectViewKey) as? UIVisualEffectView
-            if effectView == nil {
-                effectView = getNavigationBarEffectView()
-                objc_setAssociatedObject(self, &navigationBarEffectViewKey, effectView, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-            }
-            return effectView
-        }
-    }
-    
-    private func getNavigationBarEffectView() -> UIVisualEffectView? {
-        if let subviews = navigationController?.navigationBar.subviews.first?.subviews {
-            for view in subviews {
-                if view is UIVisualEffectView {
-                    return view as? UIVisualEffectView
-                }
-            }
-        }
-        return nil
-    }
 }
