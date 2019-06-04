711034553aba52bf14a1a74765254c3f5e25a2d6
lixz <lixiangzhouno1@163.com>
2019-06-04 17:04:59 +0800

    格式化日志输出

--- patient/Class/Vender/LogExtension/NSDictionary+HYBUnicodeReadable.m
+++ patient/Class/Vender/LogExtension/NSDictionary+HYBUnicodeReadable.m
@@ -33,13 +33,13 @@
         
         if ([obj isKindOfClass:[NSString class]])
         {
-            (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: \"%@\"\n", tab, key, obj] : [desc appendFormat:@"%@\t%@: \"%@\",\n", tab, key, obj];
+            (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = \"%@\"\n", tab, key, obj] : [desc appendFormat:@"%@\t%@ = \"%@\",\n", tab, key, obj];
         }
         else if ([obj isKindOfClass:[NSArray class]]
                    || [obj isKindOfClass:[NSDictionary class]]
                    || [obj isKindOfClass:[NSSet class]])
         {
-            (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: %@\n", tab, key, [obj descriptionWithLocale:locale indent:level + 1]] :  [desc appendFormat:@"%@\t%@: %@,\n", tab, key, [obj descriptionWithLocale:locale indent:level + 1]];
+            (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = %@\n", tab, key, [obj descriptionWithLocale:locale indent:level + 1]] :  [desc appendFormat:@"%@\t%@ = %@,\n", tab, key, [obj descriptionWithLocale:locale indent:level + 1]];
         }
         else if ([obj isKindOfClass:[NSData class]])
         {
@@ -56,28 +56,28 @@
                     || [result isKindOfClass:[NSSet class]])
                 {
                     NSString *str = [((NSDictionary *)result) descriptionWithLocale:locale indent:level + 1];
-                    (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: %@\n", tab, key, str] : [desc appendFormat:@"%@\t%@: %@,\n", tab, key, str];
+                    (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = %@\n", tab, key, str] : [desc appendFormat:@"%@\t%@ = %@,\n", tab, key, str];
                 }
                 else if ([obj isKindOfClass:[NSString class]])
                 {
-                    (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: \"%@\"\n", tab, key, result] : [desc appendFormat:@"%@\t%@: \"%@\",\n", tab, key, result];
+                    (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = \"%@\"\n", tab, key, result] : [desc appendFormat:@"%@\t%@ = \"%@\",\n", tab, key, result];
                 }
             }
             else {
                 @try {
                     NSString *str = [[NSString alloc] initWithData:obj encoding:NSUTF8StringEncoding];
                     if (str != nil) {
-                        (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: \"%@\"\n", tab, key, str] : [desc appendFormat:@"%@\t%@: \"%@\",\n", tab, key, str];
+                        (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = \"%@\"\n", tab, key, str] : [desc appendFormat:@"%@\t%@ = \"%@\",\n", tab, key, str];
                     } else {
-                        (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: %@\n", tab, key, obj] : [desc appendFormat:@"%@\t%@: %@,\n", tab, key, obj];
+                        (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = %@\n", tab, key, obj] : [desc appendFormat:@"%@\t%@ = %@,\n", tab, key, obj];
                     }
                 }
                 @catch (NSException *exception) {
-                    (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: %@\n", tab, key, obj] : [desc appendFormat:@"%@\t%@: %@,\n", tab, key, obj];
+                    (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = %@\n", tab, key, obj] : [desc appendFormat:@"%@\t%@ = %@,\n", tab, key, obj];
                 }
             }
         } else {
-            (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@: %@\n", tab, key, obj] : [desc appendFormat:@"%@\t%@: %@,\n", tab, key, obj];
+            (i == (allKeys.count-1)) ? [desc appendFormat:@"%@\t%@ = %@\n", tab, key, obj] : [desc appendFormat:@"%@\t%@ = %@,\n", tab, key, obj];
         }
     }
     
