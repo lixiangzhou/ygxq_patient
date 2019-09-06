ae2a8cfbadbc856721519ec10e1ca6cc889bf2a3
lixiangzhou <lixiangzhouno1@163.com>
2019-09-06 09:56:39 +0800

    移除无用库

---
+++ patient/Class/Modules/FUVisitPlan/Controller/FUVisitPlanController.swift
@@ -0,0 +1,66 @@
+//
+//  FUVisitPlanController.swift
+//  patient
+//
+//  Created by lixiangzhou on 2019/9/6.
+//Copyright © 2019 sphr. All rights reserved.
+//
+
+import UIKit
+
+class FUVisitPlanController: BaseController {
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUI()
+    }
+
+    // MARK: - Public Property
+    
+    // MARK: - Private Property
+    
+}
+
+// MARK: - UI
+extension FUVisitPlanController {
+    private func setUI() {
+        
+    }
+}
+
+// MARK: - Action
+extension FUVisitPlanController {
+    
+}
+
+// MARK: - Network
+extension FUVisitPlanController {
+    
+}
+
+// MARK: - Delegate Internal
+
+// MARK: -
+
+// MARK: - Delegate External
+
+// MARK: -
+
+// MARK: - Helper
+extension FUVisitPlanController {
+    
+}
+
+// MARK: - Other
+extension FUVisitPlanController {
+    
+}
+
+// MARK: - Public
+extension FUVisitPlanController {
+    
+}
+
